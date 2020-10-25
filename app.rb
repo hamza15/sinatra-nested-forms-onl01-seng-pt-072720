8a35b6b55bdb06dda10d9bd5c47bf85ab7495b62
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-25 18:23:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ module FormsLab
       @ship1 =Ship.new(params["pirate"]["ships"][0]["name"], params["pirate"]["ships"][0]["type"], params["pirate"]["ships"][0]["booty"])
       @ship2 =Ship.new(params["pirate"]["ships"][1]["name"], params["pirate"]["ships"][1]["type"], params["pirate"]["ships"][1]["booty"])
     end
+    erb :'pirates/show'
     
   end
 end
