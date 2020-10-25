c37b8d116a7a895b308a76f2ea87a590f9c1ea64
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-25 17:30:32 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty_attributes
+  
+  @@all = []
+  
+  def initialize(name:, weight:, height:)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all < self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
